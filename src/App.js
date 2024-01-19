5924abc8b6a151379cc41a930ba0e8787ab51044
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2024-01-19 23:01:32 +0100

    Small css updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import SearchBar from "./components/SearchBar"
 import ImageList from "./components/ImageList"
 import searchImages from './api';
 import { useState } from "react";
+import './style.css';
 
  function App() {
   const [images, setImages] = useState([]);
@@ -11,10 +12,8 @@ import { useState } from "react";
     setImages(result);
   };
 
-
-
   return (
-    <div >
+    <div className="container">
       <SearchBar onSubmit={handleSubmit}/>
       <ImageList images={images}/>
     </div>
